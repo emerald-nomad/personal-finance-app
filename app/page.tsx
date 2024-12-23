6e30d2e964a9bb808722b064845a1cd27a0d83a9
Jalen Greene <terrell.greene95@gmail.com>
2024-12-23 09:14:11 -0600

    input component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,52 @@
-import { Button } from "@/components/Button";
-import Image from "next/image";
-
+import { Input } from "@/components/Input";
+import SearchIcon from "@/images/icon-search.svg";
+// import PrefixIcon from "@/images/mo"
 export default function Home() {
   return (
-    <div 
-      style={{ 
+    <div
+      style={{
         height: "100%",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         flexDirection: "column",
-        gap: "1rem"
+        gap: "1rem",
       }}
     >
-      <Button>Placeholder</Button>
-      <Button color="secondary">Placeholder</Button>
-      <Button color="destroy">Placeholder</Button>
-      <Button color="tertiary">Placeholder</Button>
+      <Input
+        label="Basic Field"
+        id="Test"
+        placeholder="Placeholder"
+        helperText="Helper text"
+      />
+      <Input
+        label="Field with icon"
+        id="IconField"
+        placeholder="Placeholder"
+        helperText="Helper text"
+        suffixIcon={<SearchIcon />}
+      />
+      <Input
+        label="Field with prefix"
+        id="prefix"
+        placeholder="Placeholder"
+        helperText="Helper text"
+        prefix="$"
+      />
+      <Input
+        label="Field with color tag"
+        id="color-tag"
+        placeholder="Placeholder"
+        helperText="Helper text"
+        colorTag="green"
+      />
+      <Input
+        label="Field with color tag red"
+        id="color-tag-red"
+        placeholder="Placeholder"
+        helperText="Helper text"
+        colorTag="red"
+      />
     </div>
   );
 }
---
+++ components/Input/input.tsx
@@ -0,0 +1,58 @@
+import { InputHTMLAttributes, JSX } from "react";
+import styles from "./input.module.scss";
+import clsx from "clsx";
+import ColorTagIcon from "@/images/icon-color-tag.svg";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  id: string;
+  label: string;
+  type?: "text" | "select";
+  helperText?: string;
+  suffixIcon?: JSX.Element;
+  colorTag?: "green" | "red";
+}
+
+export function Input({
+  label,
+  id,
+  helperText,
+  type = "text",
+  suffixIcon,
+  prefix,
+  colorTag,
+  ...props
+}: InputProps) {
+  return (
+    <div className={clsx([styles["container"]])}>
+      <label
+        htmlFor={id}
+        className={clsx([styles["label"], "text-preset-5-bold"])}
+      >
+        {label}
+      </label>
+      <div className={clsx([styles["input-wrapper"]])}>
+        {colorTag && (
+          <ColorTagIcon
+            className={clsx([styles["color-tag"], styles[colorTag]])}
+          />
+        )}
+        {prefix && (
+          <span className={clsx([styles["prefix"], "text-preset-4"])}>
+            {prefix}
+          </span>
+        )}
+        <input
+          className={clsx([styles["input"], "text-preset-4"])}
+          id={id}
+          {...props}
+        />
+        {suffixIcon}
+      </div>
+      {helperText && (
+        <span className={clsx([styles["helper-text"], "text-preset-5"])}>
+          {helperText}
+        </span>
+      )}
+    </div>
+  );
+}
