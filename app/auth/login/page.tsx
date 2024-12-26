cdee9da62ff7a229076a2e92f31a14e48eb06bd9
Jalen Greene <terrell.greene95@gmail.com>
2024-12-26 12:13:28 -0600

    login

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,3 +1,27 @@
+import clsx from "clsx";
+import styles from "./login.module.scss";
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>Page</h1>
+  return (
+    <div className={clsx([styles["login-container"]])}>
+      <p className={clsx(["text-preset-1"])}>Login</p>
+      <div className={clsx([styles["input-group"]])}>
+        <Input id="email" label="Email" />
+        <Input id="password" label="Password" />
+      </div>
+      <Button>Login</Button>
+      <p className={clsx([styles["text"], "text-preset-4"])}>
+        Need to create an account?{" "}
+        <Link
+          href="/auth/signup"
+          className={clsx(["text-preset-4-bold", styles["link"]])}
+        >
+          Sign Up
+        </Link>
+      </p>
+    </div>
+  );
 }
