85edccdbc87bc1f2a341d9f1ad5606a90e7c3662
Jalen Greene <terrell.greene95@gmail.com>
2024-12-30 11:12:29 -0600

    overview bills

--- app/dashboard/_components/Bills/bills.tsx
+++ app/dashboard/_components/Bills/bills.tsx
@@ -11,6 +11,28 @@ export function Bills() {
           See Details
         </Button>
       </div>
+      <div className={clsx([styles["content"]])}>
+        <BillType name="Paid Bills" value={190} color="green" />
+        <BillType name="Total Upcoming" value={194.98} color="yellow" />
+        <BillType name="Coming Soon" value={59.98} color="cyan" />
+      </div>
+    </div>
+  );
+}
+
+interface BillTypeProps {
+  color: "green" | "yellow" | "cyan";
+  name: string;
+  value: number;
+}
+
+export function BillType({ name, value, color }: BillTypeProps) {
+  return (
+    <div className={clsx([styles["bill-type"], styles[color]])}>
+      <span className={clsx([styles["name"], "text-preset-4"])}>{name}</span>
+      <span className={clsx([styles["value"], "text-preset-4-bold"])}>
+        ${value.toFixed(2)}
+      </span>
     </div>
   );
 }
